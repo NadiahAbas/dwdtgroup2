11fc5a0f8e4a644bd7d1699ef3cadded6b4bda59
NadiahAbas <59683580+NadiahAbas@users.noreply.github.com>
2020-11-07 08:39:00 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">SYAFIQ</a>
+                    <a href="https://laravel.com/docs">BAHIT</a>
                     <a href="https://laracasts.com">MENGAKUN</a>
                     <a href="https://laravel-news.com">YA</a>
                     <a href="https://blog.laravel.com">SUKA</a>
